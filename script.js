4348ec051ece9d61429131725e6997f60dc430ed
Ariana Toscano Villalba <4darianatoscano@gmail.com>
2026-03-05 13:56:31 +0100

    Rediseño con Tailwind CSS y modo oscuro

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function mostrarMensaje() {
   alert("¡Prepárate para tu próxima aventura!");
 }
+const toggleDark = document.getElementById("toggleDark");
+
+toggleDark.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+});
